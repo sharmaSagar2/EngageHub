e5aefc48e15207179afb0ae69801841b8872b199
Sharmasagar2 <sagarkush2003@gmail.com>
2024-11-13 01:10:03 +0530

    added InMemoryStore

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import {server as WebSocketServer} from "websocket"
+import http from 'http'
+
+const server = http.createServer(function(request:any, response:any) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+const wsServer = new WebSocketServer({
+    httpServer: server,
+    autoAcceptConnections: false
+});
+
+function originIsAllowed(origin: string) {
+  return true;
+}
+
+wsServer.on ('request', function(request) {
+    if (!originIsAllowed(request.origin as string)) {
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+    
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
